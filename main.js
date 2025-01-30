fff8bf7044fcd76e171d4e13d6769f27eb7ba32a
Giulio <giuliopanza2000@gmail.com>
2025-01-30 22:08:31 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -18,31 +18,90 @@ ed il titolo abbia una dimensione adeguata */
 
 const row = document.querySelector(`.row`)
 
+let container = document.querySelector(`.cont`)
+
+
 fetch("https://lanciweb.github.io/demo/api/pictures/", { method: "GET" })
 .then(response => response.json())
 .then(data => {
     // codice per far qualcosa con la risposta
     
     console.log(data)
-        data.forEach(element => {
-    
-            row.innerHTML += `
-            <div class="col-lg-3 col-md-5 col-sm-12 m-2 p-2 bg-white">
-                <div class="d-flex justify-content-center align-items-center">
-                    <button class="btn1"></button>
-                </div>
-                <img src="${element.url}" alt="immagine">
-                <h3>${element.title}</h3>
-                <span>${element.date}</span>
-            </div>`
-            
-        });
 
+    let arraycol = []
+
+    data.forEach(element => {    
+         row.innerHTML += `
+         <div class="col-lg-3 col-md-5 col-sm-12 m-2 p-2 bg-white position-relative">
+             <img src="./img/pin.svg" alt="immagine" class="position-absolute top-0 start-50 translate-middle">
+             <img src="${element.url}" alt="immagine" id="imgcard">
+             <h3>${element.title}</h3>
+             <span>${element.date}</span>
+         </div>`
+         arraycol = document.querySelectorAll(`.col-lg-3`)
+    });
+
+    console.log(`${arraycol}`)
+
+    arraycol.forEach(element =>{
+        element.addEventListener('click',function (){
+
+            container.innerHTML =`
+                <div class="position-fixed bg-secondary top-0 start-0 bottom-0 end-0 d-flex justify-content-center align-items-center flex-column">
+                    <img src="" alt="immagine" id="imgover">
+                   <input type="reset" value="Chiudi" id="chiudi">
+                </div>`
+
+                container.classList.remove("d-none")
+
+                let btn = document.getElementById(`chiudi`)
+
+                btn.addEventListener('click',function (){
+
+                    container.classList.add("d-none")
+
+                console.log(`ciao`)
+                })
+         })
+     })
 })
 .catch(error => {
     // codice da eseguire in caso di errore
     console.error(error);
 });
+
+
+
+
+/*CONSEGNA
+Milestone 1
+Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, 
+isponiamo un’immagine qualunque ed un button di chiusura.
+Milestone 2
+Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+Milestone 3
+Inseriamo il pezzo di logica finale: quando una foto viene cliccata,
+dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
+Ci sono diversi modi di farlo, prova a sperimentare :faccia_leggermente_sorridente:
+Bonus
+Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta, 
+l tutto in manierà fluida. Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
+*/
+
+
+
+/*stringa.addEventListener( "mouseover", () => {
+    // stringa.style.color = "red"
+
+    if( stringa.classList == "text-danger" ){
+        stringa.classList.remove("text-danger")
+    } else {
+        stringa.classList.add("text-danger")
+    }
+
+} )*/
 
 
 
